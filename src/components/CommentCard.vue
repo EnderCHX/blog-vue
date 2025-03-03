55d9d066c088809d04811343025ee57353324102
EnderCHX <c@chxc.cc>
2025-03-03 11:54:55 +0800

    send comment

--- src/components/CommentCard.vue
+++ src/components/CommentCard.vue
@@ -1,10 +1,12 @@
-<script setup xmlns="http://www.w3.org/1999/html">
+<script setup>
 import {ref, onMounted, watch  } from 'vue'
 import global from "@/config/global.js";
 import dayjs from "dayjs";
+import { useMessage } from 'naive-ui';
 
 const textarea = ref('')
 const comments = ref([])
+const message = useMessage()
 
 const props = defineProps({
   passage_id: {
@@ -17,6 +19,33 @@ const GetComments = async () => {
   const response = await fetch(`${global.blogApiUrl}/comments/passage/${props.passage_id}`)
   const data = await response.json()
   comments.value = data.data
+}
+
+const SendComment = async () => {
+  if (textarea.value === '') {
+    message.warning('评论不能为空')
+    return
+  }
+  let url = `${global.blogApiUrl}/newcomment`
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${sessionStorage.getItem('access_token')}`
+    },
+    body: JSON.stringify({
+      passage_id: props.passage_id,
+      content: textarea.value
+    })
+  })
+  const data = await response.json()
+  if (data.code === 200) {
+    message.success('评论成功')
+    textarea.value = ''
+    GetComments()
+  } else {
+    message.error('评论失败')
+  }
 }
 
 const FormatDate = (date) => {
@@ -47,7 +76,7 @@ watch(
         type="textarea"
         placeholder="输入文字，支持markdown"
     />
-    <n-button type="primary" @click="NewComment">评论</n-button>
+    <n-button type="primary" @click="SendComment">评论</n-button>
     <n-card v-for="(comment, index) in comments " :key="index">
       <n-text>
         {{ comment.username }} <br>
